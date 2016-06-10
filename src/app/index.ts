de83f4e58ca00c97cea0be07e9fc7d3e8fcc4bee
Brandon Roberts <robertsbt.ags@gmail.com>
2016-06-10 15:42:57 -0500

    Replicated ng serve -prod bug

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,2 @@
 export * from './environment';
 export * from './ngrx-router-cli-sample.component';
-export * from './routes';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { NgrxRouterCliSampleAppComponent, environment, routes } from './app/';
+import { NgrxRouterCliSampleAppComponent, environment } from './app/';
 import { provideRouter } from '@ngrx/router';
+import { routes } from './app/routes';
 
 if (environment.production) {
   enableProdMode();
